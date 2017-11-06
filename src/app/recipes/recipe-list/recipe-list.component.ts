6a7e053f59774ef5c4fb23c3d3527ef365add25f
realWallawalla <timon.sarakinis@gmail.com>
2017-11-06 10:25:21 +0100

    made a list and added section for adding item to list.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('walla', 'wallawalla', 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg'),
+              new Recipe('walla', 'wallawalla', 'https://ichef-1.bbci.co.uk/news/660/cpsprodpb/1325A/production/_88762487_junk_food.jpg') ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListtComponent implements OnInit {
-
+  ingredients: Ingredient [] = [new Ingredient('walla', 8),
+                                new Ingredient('walla', 8) ];
   constructor() { }
 
   ngOnInit() {
